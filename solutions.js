9ed0acf0bbf1a0af4e41bb397320316380a2c0be
Nicholas Crispino <crispinonicky@gmail.com>
2019-04-09 12:50:48 -0400

    more puzzles

--- solutions.js
+++ solutions.js
@@ -18,3 +18,79 @@ function getMiddle(s){
   
 }
 
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for
+// different data types.
+
+function findShort(s){
+
+
+  let shortest = 99999
+  let array = s.split(" ")
+  
+  for (i = 0; i < array.length; i++){
+    if (array[i].length < shortest) {
+      shortest = array[i].length
+    }
+  }
+  
+  return shortest
+//   console.log(array)
+  
+}
+
+// In this little assignment you are given a string of space separated numbers, 
+// and have to return the highest and lowest number.
+
+function highAndLow(numbers){
+  
+  let numArr = numbers.split(" ")
+  
+//   console.log(numArr)
+  
+  for (i = 0; i < numArr.length; i++){
+    numArr[i] = parseInt(numArr[i])
+  }
+  
+  
+  numArr.sort(function(a, b){return a-b});
+  
+  console.log(numArr)
+  
+  return (numArr[numArr.length - 1] + " " + numArr[0])
+  
+}
+
+// Create a function that returns the sum of the two lowest positive 
+// numbers given an array of minimum 4 integers. No floats or empty 
+// arrays will be passed.
+
+function sumTwoSmallestNumbers(numbers) { 
+
+  let small1 = 9999999999
+  let small2 = 9999999999
+   
+   for (i = 0; i < numbers.length; i++) {
+     if (numbers[i] < small1){
+       small1 = numbers[i]
+     }
+     
+   }
+   
+  //  console.log(small1)
+  //  console.log(small2)
+   
+   for (i = 0; i < numbers.length; i++){
+     if(numbers[i] < small2 && numbers[i] != small1){
+       small2 = numbers[i]
+       }
+   }
+   
+   
+   return small1 + small2
+   
+   
+  };
+
+  
