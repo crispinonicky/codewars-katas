2abf8219da16481eea62423db3f4b38160f4b464
Nicholas Crispino <crispinonicky@gmail.com>
2019-04-10 11:49:52 -0400

    pig latin

--- solutions.js
+++ solutions.js
@@ -203,4 +203,35 @@ function highAndLow(numbers){
 }
 
 
+// Move the first letter of each word to the end of it, then add "ay" to the end
+// of the word. Leave punctuation marks untouched.
+
+
+function pigIt(str){
+  
+  let wordArr = str.split(" ")
+  
+//   console.log(wordArr)
+  
+  let map1 = wordArr.map(x => x.substring(1, x.length) +  x[0] + 'ay');
+  
+  
+  for (i = 0; i < map1.length; i++) {
+    if (map1[i].length === 3 && (map1[i][0] !== 'o')){
+      console.log(map1[0][0])
+      map1[i] = map1[i][0]
+    } 
+  }
+  
+  for (i = 0; i < map1.length; i++) {
+    if (map1[i] === "O") {
+      map1[i] = "Oay"
+    }
+  }
+  
+  console.log(map1)
+  
+  return map1.join(" ")
+  
+}
 
