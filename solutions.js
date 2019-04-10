1b50e0d8c444bdcefd2c28ec1d10d5185ef19952
Nicholas Crispino <crispinonicky@gmail.com>
2019-04-10 12:45:21 -0400

    parentheses puzzle

--- solutions.js
+++ solutions.js
@@ -264,3 +264,34 @@ function humanReadable(seconds) {
 return(hours +":" + mins + ":" + secs)
   
 }
+
+// Write a function called that takes a string of parentheses, and determines 
+// if the order of the parentheses is valid. The function should return true if 
+// the string is valid, and false if it's invalid.
+
+function validParentheses(parens){
+
+  if (parens[0] === ")" || parens[parens.length - 1] == "(") {
+    return false
+  }
+  
+  let parenCounter = 0
+  
+  for (i = 0; i < parens.length; i++) {
+    if (parens[i] == "(") {
+      parenCounter++
+    } else {
+      parenCounter--
+    }
+    
+  }
+  
+  if (parenCounter === 0){
+    return true
+  } else {
+    return false
+  }
+
+
+}
+
