80ccf810f20b87c22e2a60892da294a3f000366d
Nicholas Crispino <crispinonicky@gmail.com>
2019-04-15 09:18:57 -0400

    phone number

--- solutions.js
+++ solutions.js
@@ -295,3 +295,35 @@ function validParentheses(parens){
 
 }
 
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string 
+// of those numbers in the form of a phone number.
+
+function createPhoneNumber(numbers){
+  
+  let strArr = numbers
+  
+  for (i = 0; i < strArr.length; i++) {
+    strArr[i] = strArr[i].toString()
+  }
+  
+//   console.log(strArr)
+  
+  strArr[0] = "(" + strArr[0]
+  
+  strArr[2] = strArr[2] + ") "
+  
+  console.log(strArr)
+  
+  strArr[5] = strArr[5] + "-"
+  
+  let final
+  
+  for (i = 0; i < strArr.length; i++){
+    final += strArr[i]
+  }
+  
+//   return final  
+    
+  return final.slice(9)
+}
+
