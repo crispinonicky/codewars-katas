89ad699bec414f8bcadb99368f46150354b7b87a
Nicholas Crispino <crispinonicky@gmail.com>
2019-04-15 23:42:46 -0400

    easy zero challenge

--- solutions.js
+++ solutions.js
@@ -327,3 +327,17 @@ function createPhoneNumber(numbers){
   return final.slice(9)
 }
 
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving 
+// the order of the other elements.
+
+var moveZeros = function (arr) {
+  
+  let nonZero = arr.filter(item => item !== 0)
+  let zeros = arr.filter(item => item === 0) 
+  
+  console.log(nonZero)
+  
+  return(nonZero.concat(zeros))
+  
+}
+
