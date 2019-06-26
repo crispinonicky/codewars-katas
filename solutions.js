94bebd1d34b2b920d56a8c791923b9dd7f560c4d
Nicholas Crispino <crispinonicky@gmail.com>
2019-06-26 12:48:05 -0400

    div by 3

--- solutions.js
+++ solutions.js
@@ -637,3 +637,26 @@ function solution($value){
   
 };
 
+// JS Multiples of 3 puzzle:
+
+function divisibleByThree(str){
+
+  let sum = 0
+  
+  let arr = str.split("")
+  
+//   console.log(arr)
+  
+  arr.forEach(eachNum => {
+    eachNum = parseInt(eachNum)
+    sum += eachNum
+  })
+  
+  if (sum % 3 === 0) {
+    return true
+  }
+  
+  
+  return false
+
+}
