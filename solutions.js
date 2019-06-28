34c88d95f922e36c6781bf470964405958e51822
Nicholas Crispino <crispinonicky@gmail.com>
2019-06-28 12:06:14 -0400

    directions

--- solutions.js
+++ solutions.js
@@ -660,3 +660,35 @@ function divisibleByThree(str){
   return false
 
 }
+
+// Directions reduction
+
+function dirReduc(arr){
+
+  for (i = 0; i < arr.length; i++) {
+  
+    if (arr[i] == "NORTH" && arr[i+1] == "SOUTH"){
+      arr.splice(i, 2)
+      i = i - 2
+    }    
+    
+    if (arr[i] == "SOUTH" && arr[i+1] == "NORTH"){
+      arr.splice(i, 2)
+      i = i - 2
+    }    
+    
+    if (arr[i] == "EAST" && arr[i+1] == "WEST"){
+      arr.splice(i, 2)
+      i = i - 2
+    }    
+    
+    if (arr[i] == "WEST" && arr[i+1] == "EAST"){
+      arr.splice(i, 2)
+      i = i - 2
+    }
+
+  }
+    
+  return arr
+  
+}
