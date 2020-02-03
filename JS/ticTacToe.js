b7801e81d4dfd4815a824aadda033fe108abad61
Nicholas Crispino <crispinonicky@gmail.com>
2020-02-03 16:30:28 -0500

    tic tac toe finished

--- JS/ticTacToe.js
+++ JS/ticTacToe.js
@@ -1,58 +1,58 @@
 function isSolved(board) {
-  
-  console.log(board);
-  
-  
+  let unfinished = false;
+
   for (let row = 0; row < board.length; row++) {
-  
-     for (let col = 0; col < board[row].length; col++) {
-     
-//        console.log(board[row][col] )  
-       
+    for (let col = 0; col < board[row].length; col++) {
       if (board[row][col] === 0) {
-        return -1
-      } 
-      
-      if (row + 2 < board.length && col + 2 < board.length) {
-       console.log(board[row][col] )          
-        if (board[row][col] === board[row + 1][col + 1] && board[row][col] === board[row + 2][col + 2]) {
-          return board[row][col]
-          return ("3 in a row diagonally down")
+        unfinished = true;
+      } else if (board[row][col] === 1 || board[row][col] === 2) {
+        if (row + 2 < board.length && col + 2 < board.length) {
+          if (
+            board[row][col] === board[row + 1][col + 1] &&
+            board[row][col] === board[row + 2][col + 2]
+          ) {
+            console.log("3 in a row diagonally down");
+            return board[row][col];
+          }
         }
-      } 
-      if (row - 2 >=  0 && col + 2 < board.length) {
-       console.log(board[row][col] )          
-        if (board[row][col] === board[row - 1][col + 1] && board[row][col] === board[row - 2][col + 2]) {
-          return board[row][col]
-          throw ("3 in a row diagonally up")
+
+        if (row - 2 >= 0 && col + 2 < board.length) {
+          if (
+            board[row][col] === board[row - 1][col + 1] &&
+            board[row][col] === board[row - 2][col + 2]
+          ) {
+            console.log("3 in a row diagonally up");
+            return board[row][col];
+          }
         }
-      }       
-      if (col + 2 < board.length) {
-       console.log(board[row][col] )          
-        if (board[row][col] === board[row][col + 1] && board[row][col] === board[row][col + 2]) {
-          return board[row][col]
-          throw ("3 in a row horizontally")
+
+        if (col + 2 < board.length) {
+          if (
+            board[row][col] === board[row][col + 1] &&
+            board[row][col] === board[row][col + 2]
+          ) {
+            console.log("3 in a row horizontally");
+            return board[row][col];
+          }
         }
-      } 
-      if (row + 2 < board.length) {
-        console.log('checking horizontal', board[row][col] )          
-        if (board[row][col] === board[row + 1][col] && board[row][col] === board[row + 2][col]) {
-          return board[row][col]
-          throw ("3 in a row vertically")
+
+        if (row + 2 < board.length) {
+          if (
+            board[row][col] === board[row + 1][col] &&
+            board[row][col] === board[row + 2][col]
+          ) {
+            console.log("3 in a row vertically");
+            return board[row][col];
+          }
         }
-      } 
+      }
     }
-    
-       
   }
-  
-//     for (let row = 0; row < board.length; row++) {
-  
-//      for (let col = 0; col < board[row].length; col++) {
-//         if (board[row][col] === 0) {
-//           return -1
-//         }
-//       }
-//   }
-    
+
+  if (unfinished == true) {
+    console.log("There are still moves to be made");
+    return -1;
+  }
+
+  return 0;
 }
